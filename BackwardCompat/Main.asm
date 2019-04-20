a3b5fdf015927ec19838751b7bc366c72595f3b6
neogeoss <neogeo-s@hanmail.net>
2019-04-21 02:19:01 +0900

    초기 커밋.

---
+++ BackwardCompat/Main.asm
@@ -0,0 +1,20 @@
+
+.386
+.model flat
+.code
+main	proc
+		mov ax, 2
+		mov bx, 3
+		mov cx, 4
+		ret
+main endp
+end main
+;.code
+;main	proc
+		;mov rax, 2
+		;mov rbx, 3
+		;mov rcx, 4
+		;ret
+;main	endp
+;end
+;
